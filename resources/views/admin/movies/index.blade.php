758219f4a4d6487b1caaba336a02e74654d5590f
Donato-Carola <don94@live.it>
2024-02-01 18:56:08 +0100

    cards added

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -4,28 +4,57 @@
 
 @section('main-content')
 
-<h1>
+        <h1>
 
-    Movies
-</h1>
+            Movies
+        </h1>
 
-<ul>
+        <div class="container">
 
-    @forelse ($movies as $movie)
-    <li>
-        {{ $movie->title }}
-    </li>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach ($movies as $movie)
 
-    @empty
+                <div class="col">
+                  <div class="card h-100">
 
-    <li>
-        non ci sono film nella lista
-    </li>
+                    <div class="card-body">
+                        <h1>{{ $movie->title }}</h1>
+                        <h3>{{ $movie->original_title }}</h3>
+                        <h3>{{ $movie->nationality }}</h3>
+                        <h3>{{ $movie->date }}</h3>
+                    </div>
+                  </div>
+                </div>
+                @endforeach
+
+              </div>
+
+        </div>
 
 
-    @endforelse
-</ul>
 
+
+            <!--<div class="row">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-3 mb-3">
+                    @foreach ($movies as $movie)
+                        <div class="col">
+                        <div class="card">
+
+                            <div class="card-body">
+                                <h1>{{ $movie->title }}</h1>
+                                <h3>{{ $movie->original_title }}</h3>
+                                <h3>{{ $movie->nationality }}</h3>
+                                <h3>{{ $movie->date }}</h3>
+                            </div>
+                        </div>
+                </div>
+                @endforeach
+            </div>
+
+
+
+        </div>
+
+        </div>-->
 
 @endsection
-
